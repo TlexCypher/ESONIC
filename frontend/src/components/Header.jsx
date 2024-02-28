d4f2377f23c1b7bef9013ac4a2d27b0963868f0d
Taichi Araki <araki@fun.bio.keio.ac.jp>
2024-02-29 03:17:44 +0900

    modify app name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <Flex bg={"purple.400"} py="10px">
-      <Heading ml="20px" color={"white"}>English App</Heading>
+      <Heading ml="20px" color={"white"}>ESONIC</Heading>
       <Spacer />
       <HStack mr="20px">
         <Button>Logout</Button>
